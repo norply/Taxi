d88a0553de98fb490bf7072bc467c7ea085f37af
norply <75418405+norply@users.noreply.github.com>
2021-10-15 22:37:45 +0500

    Начало
    Подключаем библиотеки для использования русского языка и ввода/вывода. Подключаем русский язык, объявляем и одновременно инициализируем переменные.

---
+++ Taxi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    setlocale(LC_CTYPE, "rus");
+    float n = 0;// число сотрудников, машин.
+    int i = 0;// счётчик
+    int i1 = 0;// счётчик
+}
